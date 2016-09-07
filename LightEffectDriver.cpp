dcd9072ad502ff99ec1f727b8314c39583d489c7
Robert Knauer <robert@capsaicin-dev.de>
2016-09-07 23:48:14 +0200

    some changes

--- LightEffectDriver.cpp
+++ LightEffectDriver.cpp
@@ -1,6 +1,6 @@
 #include <LightEffectDriver.h>
 
-LightEffectDriver::LightEffectDriver(byte[] pins, byte pinNumber) {
+LightEffectDriver::LightEffectDriver(byte* pins, byte pinNumber) {
   _pinNumber = pinNumber;
   _pins = new byte[pinNumber];
   _minValues = new byte[pinNumber];
@@ -22,17 +22,20 @@ void LightEffectDriver::update() {
   };
   /* Check the effect: */
   if (_effect == SWITCH) {
-    /* TODO */
+    _indexPin1 = _chooseNextPinIndex(_indexPin1);
   } else if (_effect == FADE) {
     /* TODO */
   } else if (_effect == FADEOVER) {
     /* TODO */
-  } else {
-    /* FIXME: Invalid effect */
   };
+  /* Set pins: */
+  _setPins();
+  /* Set last change: */
+  _lastChange = millis();
 }
 
 void LightEffectDriver::setEffect(LightEffect effect, LightEffectOrder order, LightEffectDirection direction) {
+  /* TODO: Check arguments */
   _effect = effect;
   _order = order;
   _direction = direction;
@@ -46,18 +49,57 @@ void LightEffectDriver::setEffectSpeed(byte speed) {
   _calculateChangeInterval();
 }
 
-void LightEffectDriver::setMinValues(byte[] minValues) {
-  for (int i; i < pinNumber; ++i) {
+void LightEffectDriver::setMinValues(byte* minValues) {
+  for (int i; i < _pinNumber; ++i) {
     _minValues[i] = minValues[i];
   }
 }
 
-void LightEffectDriver::setMaxValues(byte[] maxValues) {
-  for (int i; i < pinNumber; ++i) {
+void LightEffectDriver::setMaxValues(byte* maxValues) {
+  for (int i; i < _pinNumber; ++i) {
     _maxValues[i] = maxValues[i];
   }
 }
 
 void LightEffectDriver::_calculateChangeInterval() {
-  _changeInterval = someAlgorithm(_speed, _effect); /* TODO */
+  // TODO: _changeInterval = someAlgorithm(_speed, _effect);
+}
+
+byte LightEffectDriver::_chooseNextPinIndex(byte currentIndex) {
+  if (_order == RANDOM) {
+    /* Random order, choose a random pin to be the next: */
+    return random(0, _pinNumber);
+  } else {
+    /* Ordered, check for UPDOWN direction: */
+    LightEffectDirection dir = _direction;
+    if (_direction == UPDOWN) {
+      dir = _directionState;
+    };
+    /* Check for effect or state direction (if effect direction is UPDOWN): */
+    if (dir == UP) {
+      /* Counting up, check if we are at the last pin: */
+      if (currentIndex == _pinNumber-1) {
+        return 0;
+      } else {
+        return currentIndex+1;
+      };
+    } else {
+      /* Counting down, check if we are at the first pin: */
+      if (currentIndex == 0) {
+        return _pinNumber;
+      } else {
+        return currentIndex-1;
+      };
+    };
+  };
+}
+
+void LightEffectDriver::_setPins() {
+  if (_effect == SWITCH) {
+    /* TODO */
+  } else if (_effect == FADE) {
+    /* TODO */
+  } else if (_effect == FADEOVER) {
+    /* TODO */
+  };
 }
--- LightEffectDriver.h
+++ LightEffectDriver.h
@@ -4,32 +4,34 @@
 
 enum LightEffect { SWITCH, FADE, FADEOVER };
 enum LightEffectOrder { ORDERED, RANDOM };
-enum LightEffectDirection { UP, DOWN, UPDOWN };
+enum LightEffectDirection { NONE, UP, DOWN, UPDOWN };
 
 class LightEffectDriver {
   public:
-    LightEffectDriver(byte[] pins, byte pinNumber);
+    LightEffectDriver(byte* pins, byte pinNumber);
     void update();
     void setEffect(LightEffect effect, LightEffectOrder order, LightEffectDirection direction);
     void setEffectSpeed(byte speed);
-    void setMinValues(byte[] minValues);
-    void setMaxValues(byte[] maxValues);
+    void setMinValues(byte* minValues);
+    void setMaxValues(byte* maxValues);
   private:
     void _calculateChangeInterval();
+    byte _chooseNextPinIndex(byte currentIndex);
+    void _setPins();
     /* Variables: */
+    byte* _pins;
     byte _pinNumber;
     byte _speed;
     LightEffect _effect;
     LightEffectOrder _order;
     LightEffectDirection _direction;
-    byte[] _pins;
-    byte[] _minValues;
-    byte[] _maxValues;
+    byte* _minValues;
+    byte* _maxValues;
     /* State Variables: */
     unsigned long _lastChange;
     unsigned long _changeInterval; /* TODO: maybe smaller datatype? */
-    byte _pin1;
-    byte _pin2; /* Only for FADEOVER effect */
+    byte _indexPin1;
+    byte _indexPin2; /* Only for FADEOVER effect */
     LightEffectDirection _directionState; /* Only for UPDOWN direction */
 };
 
