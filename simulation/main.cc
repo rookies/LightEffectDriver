b31c39213cebed5e3241da7598ebba9721b1ce21
Robert Knauer <robert@capsaicin-dev.de>
2018-10-28 22:48:56 +0100

    simulation: new example with LightEffectDriver

--- simulation/main.cc
+++ simulation/main.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <Core.hh>
 #include "Light.hh"
 #include "Simulation.hh"
 
@@ -187,10 +188,41 @@ int example2() {
     return 0;
 }
 
+/**
+ * A traffic light using the LightEffectDriverCore.
+ */
+int example3() {
+    /* Set up simulation: */
+    Simulation simulation({
+        Light(0, 0, sf::Color(255,0,0,0)),
+        Light(0, 1, sf::Color(255,255,0,0)),
+        Light(0, 2, sf::Color(0,255,0,0))
+    });
+    /* Set up driver: */
+    LightEffectDriver::Core driver(3, [&simulation](unsigned int idx, unsigned int val) {
+        simulation.setBrightness(idx, val);
+    });
+    /* Show the simulation window: */
+    if (!simulation.show()) {
+        std::cerr << "Showing the simulation failed!" << std::endl;
+        return 1;
+    }
+    /* Main loop: */
+    sf::Clock clock;
+    while (simulation.run()) {
+        if (clock.getElapsedTime().asMilliseconds() > 2) {
+            driver.update();
+            clock.restart();
+        }
+    }
+    return 0;
+}
+
 int usage(char **argv) {
     std::cerr << "Usage: " << argv[0] << " exampleId" << std::endl;
     std::cerr << "  1) Traffic light" << std::endl;
     std::cerr << "  2) Dot-matrix display" << std::endl;
+    std::cerr << "  3) Traffic light with LightEffectDriverCore" << std::endl;
     return 1;
 }
 
@@ -203,6 +235,8 @@ int main(int argc, char **argv) {
             return example1();
         case 2:
             return example2();
+        case 3:
+            return example3();
         default:
             return usage(argv);
     }
